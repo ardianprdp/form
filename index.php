670f1a218d61b435ef14a5107ffd69accdf1b3d3
ardianprdp <64082350+ardianprdp@users.noreply.github.com>
2020-08-24 11:21:36 +0700

    first commit
    baru sampai menu...

---
+++ index.php
@@ -0,0 +1,30 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="author" content="ardianprdp">
+    <meta name="description" content="Aplikasi Cetak Formulir">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="assets/css/index.css">
+    <link rel="stylesheet" href="assets/fonts/montserrat.css">
+    <link rel="shortcut icon" href="assets/images/icon.png" type="jpg/png">
+    <title>Cetak Formulir | KPP Pratama Pangkal Pinang</title>
+</head>
+<body>
+    <div class="logo">
+        <img src="assets/images/logo-white.png" alt="">
+    </div>
+
+    <div class="judul">
+        CETAK FORMULIR <br>MANDIRI
+    </div>
+
+    <div class="button">
+        <div class="btn"><span class="tombol" onclick="location.href='menu.php'">MASUK</span></div>         
+    </div>
+
+    <div class="footer">
+        <p>KPP Pratama Pangkal Pinang</p>
+    </div>
+</body>
+</html>
---
+++ menu.php
@@ -0,0 +1,36 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="author" content="ardianprdp">
+    <meta name="description" content="Aplikasi Cetak Formulir">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="assets/css/menu.css">
+    <link rel="stylesheet" href="assets/fonts/montserrat.css">
+    <link rel="shortcut icon" href="assets/images/icon.png" type="jpg/png">
+    <title>Cetak Formulir | KPP Pratama Pangkal Pinang</title>
+</head>
+<body>
+    <div class="header">
+        <div class="header-logo">
+            <img src="assets/images/logo-warna.png" alt="">        
+        </div>
+        <div class="header-title">
+            PILIH FORMULIR
+        </div>
+    </div>
+    <div class="menu-wrapper">
+        <div class="menu">
+            <div class="btn">
+                <span class="btn-menu" onclick="location.href='#'">NPWP / PKP / EFIN</span>
+            </div>
+            <div class="btn">
+                <span class="btn-menu" onclick="location.href='#'">NPWP / PKP / EFIN</span>
+            </div>
+        </div>
+    </div>
+    <div class="footer">
+        <div class="btn"><span class="btn-home" onclick="location.href='index.php'">HOME</span></div>
+    </div>
+</body>
+</html>
